cc54fab80a3cf937363d0191cc471f5fa258aee8
Vetric29 <124870526+Vetric29@users.noreply.github.com>
2023-04-14 13:57:53 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Vetric S',
+  domain: 'medangold.eu.org',
+  author: 'Vetric S',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
